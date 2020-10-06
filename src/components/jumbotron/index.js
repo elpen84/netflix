402a573f2c5d9b7aeba3165ea627393e31fe0e9a
elpen84 <elplist@gmail.com>
2020-10-05 23:22:48 -0700

    component update
    jumbotron component update

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import { Inner } from "./styles/jumbotron";
+
+export default function Jumbotron({}) {
+  return (
+    <Inner>
+      <p>Hello Again</p>
+    </Inner>
+  );
+}
