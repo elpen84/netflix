e60cdb4fb6c9b8c3a069fd140e816cdfc98b68ac
elpen84 <elplist@gmail.com>
2020-10-17 19:31:11 -0700

    footer styled
    styled footer component

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -1,38 +1,53 @@
-import React from "react";
-import {
-  Container,
-  Row,
-  Column,
-  Link,
-  Title,
-  Text,
-  Break,
-} from "./styles/footer";
+import styled from "styled-components/macro";
 
-export default function Footer({ children, ...restProps }) {
-  return <Container {...restProps}>{children}</Container>;
+export const Container = styled.div`
+  display: flex;
+  padding: 70px 56px;
+  margin: auto;
+  max-width: 1000px;
+  flex-direction: column;
+
+  @media (max-width: 1000px) {
+    padding: 70px 30px;
+  }
+`;
+
+export default Column = styled.div`
+display: flex;
+flex-direction column;
+text-align: left;
+`;
+
+export const Row = styled.div`
+display: grid;
+grid-template-coumns: repeat(auto-fill, minmax(230px, 1fr))
+grid-gap: 15px;
+
+@media(max-media: 1000px;) {
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr))
 }
+`;
 
-Footer.Row = function FooterRow({ children, ...restProps }) {
-  return <Row {...restProps}>{children}</Row>;
-};
+export const Link = styled.a`
+  color: #757575;
+  margin-bottom: 20px;
+  font-size: 13px;
+  text-decoration: none;
+`;
 
-Footer.Column = function FooterColumn({ children, ...restProps }) {
-  return <Column {...restProps}>{children}</Column>;
-};
+export const Title = styled.p`
+  font-size: 16px;
+  color: #757575;
+  margin-bottom: 40px;
+`;
 
-Footer.Link = function FooterLink({ children, ...restProps }) {
-  return <Link {...restProps}>{children}</Link>;
-};
+export const Text = styled.p`
+  font-size: 13px;
+  color: #757575;
+  margin-bottom: 40px;
+`;
 
-Footer.Title = function FooterTitle({ children, ...restProps }) {
-  return <Title {...restProps}>{children}</Title>;
-};
-
-Footer.Text = function FooterTitle({ children, ...restProps }) {
-  return <Text {...restProps}>{children}</Text>;
-};
-
-Footer.Break = function FooterBreak({ children, ...restProps }) {
-  return <Break {...restProps}>{children}</Break>;
-};
+export const Break = styled.p`
+flex-basis: 100%
+height:0;
+`;
