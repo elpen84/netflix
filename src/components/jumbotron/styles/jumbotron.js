cd04be2e000e0a6c64e1b43491dbd1999b887a2e
elpen84 <elplist@gmail.com>
2020-10-17 18:24:46 -0700

    componnent added
    jumbotron container

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -31,7 +31,13 @@ export const Pane = styled.div`
   }
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  @media (max-width: 1000px) {
+    ${Item}: last-of-type h2 {
+      margin-bottom: 50px;
+    }
+  }
+`;
 export const Title = styled.h1`
   font-size: 50px;
   line-height: 1.1;
