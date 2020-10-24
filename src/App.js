1b2c45ec69c71a76b818c3c5fd6c147c218df600
elpen84 <elplist@gmail.com>
2020-10-23 19:29:37 -0700

    routing
    updating routing

--- src/App.js
+++ src/App.js
@@ -29,6 +29,16 @@ import * as ROUTES from "./constants/routes";
 export default function App() {
   return (
     <Router>
+      <Route exact path="/browse">
+        <Browse />
+      </Route>
+      <Route exact path="/signin">
+        <Signin />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
