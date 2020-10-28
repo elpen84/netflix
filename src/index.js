36e96e79bc49aa15252d29fb3c4edff91e85fc40
elpen84 <elplist@gmail.com>
2020-10-27 19:44:59 -0700

    firebase updated
    firebased connected

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
-import { App } from "./app";
+import App from "./app";
 import { GlobalStyles } from "./global-styles";
 import { FirebaseContext } from "./context/firebase";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyARcYZ2K4FKMEO8MKsrrnbmkv8QxydIvUg",
+  authDomain: "netflix-eebbe.firebaseapp.com",
+  databaseURL: "https://netflix-eebbe.firebaseio.com",
+  projectId: "netflix-eebbe",
+  storageBucket: "netflix-eebbe.appspot.com",
+  messingSenderId: "196242068527",
+  appId: "1:196242068527:web:d9b495b4cc082c044ae2da",
 };
 
 const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
   <>
-    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+    <FirebaseContext.Provider value={{ firebase }}>
       <GlobalStyles />
       <App />
     </FirebaseContext.Provider>
