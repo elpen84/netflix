13c84a45e0c7a651c968a394fb8fe6122e1b4b63
elpen84 <elplist@gmail.com>
2020-10-29 23:50:59 -0700

    Update signup.js
    Signup componnent updated

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form } from "../components";
+import { HeaderContainer } from "../containers/header";
+import * as ROUTES from "../constants/routes";
+import { FooterContainer } from "../containers/footer";
 
 export default function Signup() {
-  return <p>Hello from the sign up</p>;
+  const [firstName, setFirstName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = (password === "") | (emailAddress === "");
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="First Name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+
+            <Form.Text>
+              Already a user? <Form.Link to="/signin">Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
